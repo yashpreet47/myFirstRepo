255009983ca8131d5ce0ef36a3678cb037988ce4
yashpreet47 <yashusingh474747@gmail.com>
2025-01-08 13:22:39 +0530

    sample js file added

---
+++ hello.js
@@ -0,0 +1,7 @@
+console.log("This is written inside the hello.js file ...console.log statement ");
+
+function add(a,b){
+    console.log(`Addition of ${a} and ${b} is ${a+b} ..`);
+}
+
+add(20,30);
